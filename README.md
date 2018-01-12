fe5438dc9855d4d3eb0f75358007c12c0fd7279c
Unknown <karotka20@onet.eu>
2018-01-12 20:47:20 +0100

    App.js and README
    Add app.js with yargs and project discription in README

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Mailer
+
+Użycie
+===========
+```
+node app -send mail_list -content content -subject subject ->  Sending and saving mails from list
+
+node app -mail aaa@mail.com -content content -date 2018-01-01 00:00 -> Seting mails shedule
+
+node app -results -> Showing sended mail list ```
+
+ Wymagania
+ ----------
+ Node.js
---
+++ app.js
@@ -0,0 +1,70 @@
+ var argv = require('yargs').argv;
+
+class Application{
+  constructor(){
+
+    console.log(argv);
+
+    //Sending mails
+    if(argv.send && argv.content && argv.subject){
+      getMailListFromDB(argv.send, function (list){
+          sendMail(list, argv.subject, argv.content, function (save){
+            saveMailListIntoDB(save, argv.send, argv.content, argv.subject function (){
+              console.log('Wysłano email!');
+            }
+          })
+        })
+      }
+
+
+    //Seting mails shedule
+    if(argv.mail && argv.content && argv.date){
+      setMailSchedule(argv.mail, argv.content, argv.date, function() {
+        console.log('Dodano email!');
+        if (argv.date >= Date.now()) {
+          mailSender.send(argv.mail, argv.content, function () {
+              console.log('Wysłano email!');
+          })
+      }
+      })
+    }
+
+      //Showing sended mail list
+      if(argv.results){
+        showSendedMailList();
+      }
+}
+
+
+  getMailListFromDB(name, callback){
+
+    //>>Function<<<
+
+    callback(list);
+  }
+  sendMail(mail, content, subject, callback){
+
+    //>>Function<<<
+
+    callback(save);
+  }
+  saveMailListIntoDB(mail, content, subject, callback){
+
+    //>>Function<<<
+
+    callback();
+  }
+  setMailSchedule(mail, content, data, callback){
+
+    //>>Function<<<
+
+    callback();
+  }
+  showSendedMailList(name){
+
+    //>>Function<<<
+
+  }
+}
+
+new Application();
